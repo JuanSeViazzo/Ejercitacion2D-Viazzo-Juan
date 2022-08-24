d678fdd05b02083588296c2490ae7bcc1f633b8b
juansearg <juansviazzo@gmail.com>
2022-08-24 20:58:11 -0300

    probando mi amor por ti

---
+++ EjercitacionClase2D-Viazzo-Juan/ValidarRangos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ValidarRangos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
